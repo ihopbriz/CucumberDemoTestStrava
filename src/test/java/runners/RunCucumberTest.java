50726624f073b9b6fd0f1a873e7780fe6c524a8e
Michal Dobrzycki <dobrzycki@gmail.com>
2023-05-23 11:02:28 +0200

    Reports and runner

---
+++ src/test/java/runners/RunCucumberTest.java
@@ -0,0 +1,13 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber.html"},
+        features = {"src/test/features"},
+        glue = "stepdefinitions")
+public class RunCucumberTest {
+}
